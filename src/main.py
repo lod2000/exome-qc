e7b031124ac0f3c42437bb97eb71b3b727f8cf84
lod2000 <lod.wolflovers@gmail.com>
2017-07-03 12:30:05 -0700

    Added analysis, tracking data/panel

--- src/main.py
+++ src/main.py
@@ -14,13 +14,7 @@ df_file = os.path.join(samples_dir, 'combined.tab')
 if not os.path.isfile(df_file):
     sample_parser.combine(gt_file, bed_file, samples_dir)
 
-df = pandas.read_csv(df_file, sep='\t', low_memory=False)#, dtype=object)
-        # dtype={'CHROMOSOME': str, 'POSITION': int, 'TOTAL_CALLERS': int,\
-        # 'GENE': str, 'DNA_CHANGE': str, 'PROTEIN_CHANGE': str,\
-        # 'EXAC_FREQ_ESTIMATE': str, '1KG_FREQ_ESTIMATE': str,\
-        # 'GT_VARSCAN': str, 'GT_GATK': str, 'GT_SAMTOOLS': str,\
-        # 'GT_FREEBAYES': str, 'GT_VARDICT': str, 'SMPLE_ID': str,\
-        # 'COVERED': bool, 'REPORTABLE': bool})
+df = pandas.read_csv(df_file, sep='\t', low_memory=False)
 gt_parsed = sample_parser.parse_gt(gt_file)
 match_list = sample_parser.find_matches(next(os.walk(samples_dir))[1], gt_parsed)
 gt = pandas.concat(sample_parser.split_gt(gt_parsed, match_list)).reset_index(drop=True)
@@ -30,12 +24,37 @@ pandas.set_option('display.max_columns', 14)
 pandas.set_option('display.max_rows', 80)
 pandas.set_option('display.width', 100)
 
+analysis_df = pandas.DataFrame({'ANALYSIS':['True Positives','False Positives',\
+        'False Negatives','True Positive Rate','Positive Predictive Value',\
+        'False Negative Rate','False Discovery Rate']})
+
 for caller in sample_parser.get_caller_names(df):
     print(caller)
     # print('True positives:')
-    tp = analysis.get_true_positives(df, caller)
-    fn = analysis.get_false_negatives(tp, gt)
+    # tp = analysis.get_true_positives(df, caller)
+    # fn = analysis.get_false_negatives(tp, gt)
     # print(tp)
     # print('False negatives:')
     # print(analysis.get_false_negatives(tp, gt))
-    print('TP+FN: ' + str(tp.shape[0]+fn.shape[0]))
+    # print('TP+FN: ' + str(tp.shape[0]+fn.shape[0]))
+
+    tp = analysis.get_true_positives(df, caller)
+    fp = analysis.get_false_positives(df, caller)
+    fn = analysis.get_false_negatives(tp, gt)
+
+    tps = tp.shape[0]
+    fps = fp.shape[0]
+    fns = fn.shape[0]
+
+    tpr = tps / (tps + fns)
+    ppv = tps / (tps + fps)
+    fnr = fns / (fns + tps)
+    fdr = fps / (fps + tps)
+
+    analysis_df[caller] = [tps, fps, fns, tpr, ppv, fnr, fdr]
+
+analysis_file = os.path.join(data_path, 'analysis.csv')
+try:
+    analysis_df.to_csv(analysis_file, sep='\t', encoding='utf-8', index=False)
+except PermissionError:
+    print('Another program is using the file analysis.csv. Please close and try again.')
