3a4fc227ddbf2ca0c8035d737ddf5818e0f43d31
lod2000 <lod.wolflovers@gmail.com>
2017-07-05 10:36:54 -0700

    Fixed sample id type bug for false negatives

--- src/analysis.py
+++ src/analysis.py
@@ -12,15 +12,36 @@ def get_false_positives(df, caller_name):
             and not df[caller_name][i] == './.']].reset_index(drop=True)
 
 def get_false_negatives(tp, gt):
-    return gt.iloc[[i for i in range(0, gt.shape[0])\
-            if not (gt['GENE'][i] in tp['GENE'].tolist()\
-            and gt['DNA_CHANGE'][i] in tp['DNA_CHANGE'].tolist()\
+    return gt.iloc[[i for i in range(0, gt.shape[0])
+            if not (gt['GENE'][i] in tp['GENE'].tolist()
+            and gt['DNA_CHANGE'][i] in tp['DNA_CHANGE'].tolist()
             and gt['PROTEIN_CHANGE'][i] in tp['PROTEIN_CHANGE'].tolist()
-            and gt['SAMPLE_ID'][i] in tp['SAMPLE_ID'].tolist())]]\
-            .reset_index(drop=True)
+            and gt['SAMPLE_ID'][i] in tp['SAMPLE_ID'].tolist())]].reset_index(drop=True)
 
 # Not covered, not reportable
 def get_unclassified(df, caller_name):
     return df.iloc[[i for i, cov in enumerate(df['COVERED'])\
             if not cov and not df['REPORTABLE'][i]\
             and not df[caller_name][i] == './.']].reset_index(drop=True)
+
+# TODO does the 'end' position include the last covered position?
+# Returns a DataFrame of individual positions, genes, and chromosomes covered
+def get_positions(panel):
+    positions = []
+    genes = []
+    chromosomes = []
+    for i in range(0, panel.shape[0]):
+        pos_list = list(range(panel['START'][i], panel['END'][i]))
+        positions += pos_list
+        genes += len(pos_list) * [panel['GENE'][i]]
+        chromosomes += len(pos_list) * [panel['CHROMOSOME'][i]]
+    return pandas.DataFrame({ 'POSITION' : positions,\
+                              'GENE' : genes,\
+                              'CHROMOSOME' : chromosomes })
+
+# Returns list of positions which were not called (correctly) and were covered
+# by the small panel
+def get_true_negatives(df, caller_name, positions):
+    covered = df.iloc[[i for i, covered in enumerate(df['COVERED']) if covered\
+            and not df[caller_name][i] == './.']].reset_index(drop=True)
+    return positions.iloc[[i for i, pos in enumerate(positions['POSITION']) if not pos in covered['POSITION'].tolist()]].reset_index(drop=True)
--- src/main.py
+++ src/main.py
@@ -16,14 +16,19 @@ if not os.path.isfile(df_file):
     sample_parser.combine(gt_file, bed_file, samples_dir)
 
 df = pandas.read_csv(df_file, sep='\t', low_memory=False)
+# Convert SAMPLE_IDs to Strings
+df['SAMPLE_ID'] = df['SAMPLE_ID'].astype(str)
 gt_parsed = sample_parser.parse_gt(gt_file)
 match_list = sample_parser.find_matches(next(os.walk(samples_dir))[1], gt_parsed)
 gt = pandas.concat(sample_parser.split_gt(gt_parsed, match_list)).reset_index(drop=True)
+panel = sample_parser.parse_bed(bed_file)
 
 # DataFrame terminal display options
 pandas.set_option('display.max_columns', 14)
-pandas.set_option('display.max_rows', 80)
-pandas.set_option('display.width', 100)
+pandas.set_option('display.max_rows', 13)
+pandas.set_option('display.width', 300)
+
+print(gt)
 
 # Initialize analysis DataFrame
 analysis_df = pandas.DataFrame({'ANALYSIS':['True Positives','False Positives',\
@@ -31,30 +36,38 @@ analysis_df = pandas.DataFrame({'ANALYSIS':['True Positives','False Positives',\
         'False Negative Rate','False Discovery Rate']})
 
 for caller in sample_parser.get_caller_names(df):
+    print('\n')
     print(caller)
 
     # True positives DataFrame
     tp = analysis.get_true_positives(df, caller)
+    # True negatives in DataFrame
+    # tn = analysis.get_true_negatives(df, caller, analysis.get_positions(panel))
     # False positives DataFrame
-    fp = analysis.get_false_positives(df, caller)
+    # fp = analysis.get_false_positives(df, caller)
     # False negatives DataFrame
     fn = analysis.get_false_negatives(tp, gt)
 
     # Count rows in DataFrames
     tps = tp.shape[0]
-    fps = fp.shape[0]
+    # tns = tn.shape[0]
+    # fps = fp.shape[0]
     fns = fn.shape[0]
 
     # Analysis
-    tpr = tps / (tps + fns)
-    ppv = tps / (tps + fps)
-    fnr = fns / (fns + tps)
-    fdr = fps / (fps + tps)
+    # tpr = tps / (tps + fns)
+    # ppv = tps / (tps + fps)
+    # fnr = fns / (fns + tps)
+    # fdr = fps / (fps + tps)
 
-    analysis_df[caller] = [tps, fps, fns, tpr, ppv, fnr, fdr]
+    print(tp)
+    print(fn)
+    # print(tps+fns)
 
-analysis_file = os.path.join(data_path, 'analysis.csv')
-try:
-    analysis_df.to_csv(analysis_file, sep='\t', encoding='utf-8', index=False)
-except PermissionError:
-    print('Another program is using the file analysis.csv. Please close and try again.')
+    # analysis_df[caller] = [tps, fps, fns, tpr, ppv, fnr, fdr]
+
+# analysis_file = os.path.join(data_path, 'analysis.csv')
+# try:
+    # analysis_df.to_csv(analysis_file, sep='\t', encoding='utf-8', index=False)
+# except PermissionError:
+#     print('Another program is using the file analysis.csv. Please close and try again.')
