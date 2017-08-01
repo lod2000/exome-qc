1fc1bc0b80913c553ec8e2cfe6716b89287c9b68
lod2000 <lod.wolflovers@gmail.com>
2017-08-01 13:10:04 -0700

    added training and testing sets

--- src/main.py
+++ src/main.py
@@ -30,6 +30,11 @@ if os.path.isfile(combined_file):
     df = pandas.read_csv(combined_file, sep='\t', low_memory=False)
     # Convert SAMPLE_IDs to Strings
     df['SAMPLE_ID'] = df['SAMPLE_ID'].astype(str)
+    # Get DataFrame of covered variants
+    covered = df.iloc[[
+            i for i in range(0, df.shape[0])
+            if df['COVERED'][i] or df['REPORTABLE'][i]
+    ]].reset_index(drop=True)
 else:
     # Generate combined data file
     if os.path.isfile(df_file):
@@ -41,17 +46,21 @@ else:
     else:
         df = parser.combine('2017-06-30_NgsReviewer_master', bed_file, samples_dir)
 
-    # Add combined callers to df
-    print('Adding combined callers...')
-    analysis.add_x_or_more(df)
+    # Get DataFrame of covered variants
     covered = df.iloc[[
             i for i in range(0, df.shape[0])
             if df['COVERED'][i] or df['REPORTABLE'][i]
     ]].reset_index(drop=True)
+    # Add combined callers to df
+    print('Adding combined callers...')
+    analysis.add_x_or_more(df)
+    training = covered[int(covered.shape[0] / 2):].reset_index(drop=True)
+    print(covered.shape[0])
+    print(training.shape[0])
 
     # Generate probability combined caller
     prob_cutoffs = numpy.arange(0, 1, 0.01)
-    prob_vals = analysis.prob_fn(prob_cutoffs, covered)
+    prob_vals = analysis.prob_fn(prob_cutoffs, training)
     prob_ratios = [
             analysis.get_mcc(
                     prob_vals['TP'][i], prob_vals['TN'][i],
@@ -68,7 +77,7 @@ else:
     # Generate weights combined caller by calculating optimum cutoff
     weights = analysis.get_caller_weights(df)
     weight_cutoffs = numpy.arange(0, sum(weights), 0.01)
-    weight_vals = analysis.weight_fn(weight_cutoffs, covered, weights)
+    weight_vals = analysis.weight_fn(weight_cutoffs, training, weights)
     weight_ratios = [
             analysis.get_mcc(
                     weight_vals['TP'][i], weight_vals['TN'][i],
@@ -89,8 +98,9 @@ else:
             encoding='utf-8', index=False
     )
 
-analysis_df = analysis.analyze_callers(df, panel)
-analysis.plot_callers(df, analysis_df, output_dir)
+training = covered[:int(covered.shape[0] / 2)].reset_index(drop=True)
+analysis_df = analysis.analyze_callers(training, panel)
+analysis.plot_callers(training, analysis_df, output_dir)
 analysis_file = os.path.join(output_dir, 'analysis.csv')
 try:
     analysis_df.to_csv(
