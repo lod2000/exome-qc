07f7e2cfff93f4dec62cf325b559fc49e7808f14
lod2000 <lod.wolflovers@gmail.com>
2017-08-15 12:52:29 -0700

    removed analysis file

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import sys
 import glob
 import importlib
 import re
+import math
 
 import pandas
 import numpy
@@ -12,7 +13,6 @@ from adjustText import adjust_text
 
 import utils
 import parser
-import analysis
 
 # Directory structure
 main_dir = os.path.abspath(os.path.join(sys.path[0], '..'))
@@ -28,6 +28,7 @@ df_file = os.path.join(output_dir, 'parsed.tab')
 combined_file = os.path.join(output_dir, 'parsed_combined.tab')
 
 # Import joint caller modules
+print('Importing joint caller modules...')
 callers_dir = os.path.join(main_dir, 'callers')
 sys.path.append(callers_dir)
 caller_names = [
@@ -68,6 +69,7 @@ else:
     df = parser.combine('2017-06-30_NgsReviewer_master', bed_file, samples_dir)
 
 # Get indices of covered variants
+print('Splitting training and testing sets...')
 covered_indices = [
         i for i in range(0, df.shape[0])
         if df['COVERED'][i] or df['REPORTABLE'][i]
@@ -94,7 +96,51 @@ df.to_csv(
 testing_indices = covered_indices[:covered_split]
 testing = df.iloc[testing_indices].reset_index(drop=True)
 
-analysis_df = analysis.analyze_callers(testing, panel)
+# Analyze callers
+print('Analyzing callers...')
+# Initialize analysis DataFrame
+analysis_df = pandas.DataFrame({
+        'ANALYSIS' : ['True Positives', 'True Negatives',
+        'False Positives', 'False Negatives', 'True Positive Rate',
+        'True Negative Rate', 'Positive Predictive Value',
+        'Negative Predictive Value', 'False Negative Rate',
+        'False Positive Rate', 'False Discovery Rate',
+        'False Omission Rate',
+        'Accuracy', 'Matthews Correlation Coefficient']
+})
+callers = utils.get_all_callers(df)
+for caller in callers:
+    # Get true and false positives and negatives
+    tp_df = utils.get_tp(testing, caller)
+    fp_df = utils.get_fp(testing, caller)
+    tn_df = utils.get_tn(testing, caller)
+    fn_df = utils.get_fn(testing, caller)
+
+    # Count rows in DataFrames
+    tp = tp_df.shape[0]
+    tn = tn_df.shape[0]
+    fp = fp_df.shape[0]
+    fn = fn_df.shape[0]
+
+    # Analysis
+    tpr = tp / (tp + fn) # True positive rate (sensitivity)
+    tnr = tn / (tn + fp) # True negative rate (specificity)
+    ppv = tp / (tp + fp) # Positive predictive value (precision)
+    npv = tn / (tn + fn) # Negative predictive value
+    fnr = fn / (fn + tp) # False negative rate (miss rate)
+    fpr = fp / (fp + tn) # False positive rate (fall-out)
+    fdr = fp / (fp + tp) # False discovery rate
+    fom = fn / (fn + tn) # False omission rate
+    acc = (tp + tn) / (tp + tn + fp + fn) # Accuracy
+    # Matthews correlation coefficient
+    mcc = ((tp * tn - fp * fn)
+            / math.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)))
+
+    # Add column to analysis dataframe
+    analysis_df[caller] = [
+            tp, tn, fp, fn, tpr, tnr, ppv, npv, fnr, fpr, fdr, fom, acc, mcc
+    ]
+#analysis_df = analysis.analyze_callers(testing, panel)
 #analysis.plot_callers(testing, analysis_df, output_dir)
 analysis_file = os.path.join(output_dir, 'analysis.csv')
 analysis_df.to_csv(
@@ -103,28 +149,32 @@ analysis_df.to_csv(
 print('Output to file ' + analysis_file)
 
 # Plot callers
-matplotlib.rcParams.update({'font.size': 16})
-figure, axes = pyplot.subplots()
-callers = [c.split('_')[-1] for c in utils.get_all_callers(df)]
+print('Plotting callers...')
+# Transpose analysis DataFrame
 at = analysis_df.transpose().reset_index()
 at.rename(columns = at.iloc[0], inplace=True)
 at = at[1:].reset_index(drop=True)
+# DataFrame of original callers
 at_original = at.iloc[[
         i for i, caller in enumerate(at['ANALYSIS']) 
         if re.search('GT_', caller)
 ]] 
+# Joint callers
 at_joint = at.iloc[[
         i for i, caller in enumerate(at['ANALYSIS']) 
         if re.search('JOINT_', caller)
 ]]
 
-# Plot labels
+# Plot params
+matplotlib.rcParams.update({'font.size': 16})
+figure, axes = pyplot.subplots()
 pyplot.title('Mutation Caller ROC')
 pyplot.xlabel('False Positive Rate')
 pyplot.ylabel('True Positive Rate')
 pyplot.ylim(ymin=0)
 pyplot.xlim(xmin=0)
 
+# Plot ROC curves if applicable
 for caller in caller_modules:
     if hasattr(caller, 'get_roc'):
         vals = caller.get_roc(testing) 
@@ -140,8 +190,7 @@ for caller in caller_modules:
                 '-', 
                 label=caller.NAME
         )
-
-# Original callers scatter plot
+# Plot original callers
 axes.scatter(
         at_original['False Positive Rate'],
         at_original['True Positive Rate'],
@@ -150,7 +199,7 @@ axes.scatter(
         s=50,
         label='Original callers'
 )
-# Joint callers scatter plot
+# Plot joint callers
 axes.scatter(
         at_joint['False Positive Rate'],
         at_joint['True Positive Rate'],
@@ -160,8 +209,8 @@ axes.scatter(
         label='Joint callers'
 )
 
+# Create legend
 pyplot.legend(loc=4)
-
 # Point labels
 r = re.compile('^(GT_|JOINT_)')
 callers = [c.split('_')[-1] for c in filter(r.match, list(analysis_df))]
